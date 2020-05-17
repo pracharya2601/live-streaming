0bc27af872ab919380e2cc12b85f6676a6516531
Prakash Acharya <pracharya2601@gmail.com>
2020-05-16 23:14:34 -0700

    react setup

---
+++ client/src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
+
+
+const App = () => {
+    return (
+        <div>
+            hello
+        </div>
+    )
+};
+
+export default App;
